49446fcd336943ea6f8e338fe8d1b002405acbce
Ben <ben.sembira.1@gmail.com>
2024-03-23 13:13:54 +0200

    feat: part of or scripts

---
+++ or-scripts/camera.sh
@@ -0,0 +1,11 @@
+
+# random data
+# ffmpeg -f lavfi -i testsrc=size=1920x1080:rate=30 -c:v libx265 -crf 28 -f rtp rtp://localhost:7000
+
+
+# ffmpeg -re -i input.mp4 -map 0:v:0 -c:v libx265 -crf 28 -f rtp rtp://localhost:7000
+
+# file 
+while true; do
+    ffmpeg -i input.mp4 -map 0:v:0 -c:v libx265 -crf 28 -f rtp rtp://localhost:7000
+done
